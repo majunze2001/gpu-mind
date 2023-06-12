bc51fea9dcb237b820459f772c475cab9f21d238
Jeff Ma <jm9527@nyu.edu>
2023-06-12 17:04:12 -0400

    exclude gtc

--- test_trace.sh
+++ test_trace.sh
@@ -1,15 +1,15 @@
 
 # gtc
-sudo dmesg -c
-echo "" > trace
-echo 1 > tracing_on
-/home/sslee/gpu-mind/gtc.o > /home/sslee/gpu-mind/gtc.out
-echo 0 > tracing_on
-sudo dmesg > /home/sslee/gpu-mind/gtc.printk
-cp /sys/kernel/debug/tracing/trace /home/sslee/gpu-mind/gtc.tracing
-start=$(grep -n 'uvm' /home/sslee/gpu-mind/gtc.tracing | head -n 1 | cut -d: -f1)
-end=$(grep -n 'uvm' /home/sslee/gpu-mind/gtc.tracing | tail -n 1 | cut -d: -f1)
-sed -n "${start},${end}p" /home/sslee/gpu-mind/gtc.tracing > /home/sslee/gpu-mind/uvm_gtc_tracing
+# sudo dmesg -c
+# echo "" > trace
+# echo 1 > tracing_on
+# /home/sslee/gpu-mind/gtc.o > /home/sslee/gpu-mind/gtc.out
+# echo 0 > tracing_on
+# sudo dmesg > /home/sslee/gpu-mind/gtc.printk
+# cp /sys/kernel/debug/tracing/trace /home/sslee/gpu-mind/gtc.tracing
+# start=$(grep -n 'uvm' /home/sslee/gpu-mind/gtc.tracing | head -n 1 | cut -d: -f1)
+# end=$(grep -n 'uvm' /home/sslee/gpu-mind/gtc.tracing | tail -n 1 | cut -d: -f1)
+# sed -n "${start},${end}p" /home/sslee/gpu-mind/gtc.tracing > /home/sslee/gpu-mind/uvm_gtc_tracing
 
 # ctg
 sudo dmesg -c
