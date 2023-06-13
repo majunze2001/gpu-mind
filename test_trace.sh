1aa4467626c7bd5003b2a2a1799abe3b9e2c070d
Jeff Ma <jm9527@nyu.edu>
2023-06-12 22:46:25 -0400

    update test script

--- test_trace.sh
+++ test_trace.sh
@@ -6,6 +6,15 @@ echo 'arch_cpu_idle_enter' | sudo tee -a set_ftrace_notrace
 echo 'rcu_idle_enter' | sudo tee -a set_ftrace_notrace
 echo 'smp_apic_timer_interrupt' | sudo tee -a set_ftrace_notrace
 echo 'hrtimer_interrupt' | sudo tee -a set_ftrace_notrace
+echo 'mutex_lock' | sudo tee -a set_ftrace_notrace
+echo 'mutex_unlock' | sudo tee -a set_ftrace_notrace
+echo 'down_read' | sudo tee -a set_ftrace_notrace
+echo 'down_write' | sudo tee -a set_ftrace_notrace
+# uvm
+echo 'uvm_va_space_can_read_duplicate' | sudo tee -a set_ftrace_notrace
+echo 'grow_fault_granularity_if_no_thrashing' | sudo tee -a set_ftrace_notrace
+echo 'uvm_va_policy_is_read_duplicate' | sudo tee -a set_ftrace_notrace
+echo 'block_region_authorized_processors' | sudo tee -a set_ftrace_notrace
 
 # gtc
 # sudo dmesg -c
